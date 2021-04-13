3b947179ca360daa65e5c656573a861db435b3a3
hengyunabc <hengyunabc@gmail.com>
2021-04-13 17:32:25 +0800

    add HelloController

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello " + new Date();
+    }
+
+}
